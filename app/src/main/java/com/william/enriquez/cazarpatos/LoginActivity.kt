c38472080f7a98bbec96334041701ab802891a63
William Esteban Enriquez Recalde <williamenriquez526@gmail.com>
2025-05-22 12:01:43 -0500

    Cambios Pantalla de Login

---
+++ app/src/main/java/com/william/enriquez/cazarpatos/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.william.enriquez.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    fun btnLoginOnClick(view:View){
+
+        val intent = Intent(this, MainActivity::class.java) //esto es para cambiar entre pantallas
+        startActivity(intent) //esto es para cambiar entre pantallas
+    }
+}
